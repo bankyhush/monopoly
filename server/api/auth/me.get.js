bcfacf7390d633900dd71bd761ece2ae5f22ff7b
darkzuko315 <officialbankyhush@gmail.com>
2025-09-17 16:40:36 +0100

    OK

--- server/api/auth/me.get.js
+++ server/api/auth/me.get.js
@@ -1,1 +1,10 @@
-export default defineEventHandler((event) => {});
+import { getAuthUser } from "../../utils/getAuthUser";
+
+export default defineEventHandler(async (event) => {
+  const user = await getAuthUser(event);
+  if (!user) {
+    throw createError({ statusCode: 401, message: "Not authenticated" });
+  }
+
+  return user;
+});
