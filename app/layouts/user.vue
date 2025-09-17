a1847c2f139ac182df783b34c4de2555a9455fc5
darkzuko315 <officialbankyhush@gmail.com>
2025-09-17 13:30:03 +0100

    ROle Added TO User Layout

---
+++ app/layouts/user.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex h-screen bg-[#0f172a] text-gray-100 font-sans antialiased">
+    <!-- Sidebar -->
+    WELCOME USER DASHBOARD
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  middleware: ["auth.global", "role.user"],
+});
+</script>
